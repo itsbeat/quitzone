25ef93132226f4cfe4adad7d98bc942d649dc11c
Scompa00 <albertoscomparin6@gmail.com>
2020-11-30 13:18:09 +0100

    nonsochestofacendo

--- client/src/views/TeachersStudentsDetails.vue
+++ client/src/views/TeachersStudentsDetails.vue
@@ -1,22 +1,71 @@
 <template>
-  <div>
-     <router-view></router-view>
-    <table>
-      <tr>
-        <th>Nome</th>
-        <th>Azioni</th>
-      </tr>
-      <tr v-for="user in users" :key="user.id">
-        <td>{{ user.name }}</td>
-        <td>
-          <!-- <a @click="viewUser(user.id)"> Dettaglio </a> -->
-          <!-- <a class="cursor-pointer"  @click="editUser(user.id)"> 🖊️ </a> -->
-        </td>
-      </tr>
-    </table>
+  <div class="box-content h-32 w-500 p-20 border-6">
+    <router-view></router-view>
+      <div>
+        <div class="flex flex-col">
+          <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"> 
+                <table class="min-w-full divide-y divide-gray-200">
+                  <tr>
+                    <th>Esami</th> 
+                    <th>Quiz Svolti</th> 
+                  </tr>
+                  <tr>
+                    <th scope="col" class="px-3 py-2 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Esame</th>
+                    <th scope="col" class="px-3 py-2 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">N^ Domande</th>
+                    <th scope="col" class="px-3 py-2 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Risposte Corrette</th>
+                    <th scope="col" class="px-3 py-2 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Risposte Sbagliate</th>
+                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Voto Finale</th>
+                  </tr>
+                        <!-- <tr v-for="user in users" :key="user.id"> -->
+                        <!-- <td>{{ user.name }}</td> -->
+                        <!-- <a @click="viewUser(user.id)"> Dettaglio </a> -->
+                        <!-- <a class="cursor-pointer"  @click="editUser(user.id)"> 🖊️ </a> -->
+                  <td> Cyber Security </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 30 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 20 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 10 </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">15/100</a>
+                  </td>      
 
+                  <tr>
+                  <td> Cyber Security </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 30 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 20 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 10 </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">15/100</a>
+                  </td>      
+                  </tr>
+                  <tr>
+                  <td> Cyber Security </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 30 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 20 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 10 </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">15/100</a>
+                  </td>      
+                  </tr>
+                  <tr>
+                  <td> Cyber Security </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 30 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 20 </td>
+                  <td class="px-3 py-2 whitespace-nowrap text-left text-sm font-medium"> 10 </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">15/100</a>
+                  </td>      
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
+
 
 <script>
 //import axios from "axios";
