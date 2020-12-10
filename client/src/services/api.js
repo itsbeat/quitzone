3046e3d15370dffd7a014e3a8fc0c52fe3354ea7
Giacomo Alberini <giacomoalbe@gmail.com>
2020-12-10 18:12:03 +0100

    Fix wrong baseUrl for API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import router from '../router';
 
-const baseUrl = 'http://127.0.0.1:8000/api';
+const baseUrl = '/api';
 
 const api = axios.create({
     baseURL: `${baseUrl}`,
