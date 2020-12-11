8548bb22e72a978a9dc42590b443001df368f574
michipase <michipase@gmail.com>
2020-12-11 10:22:22 +0100

    add route in app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/its">ITS</router-link>
+      <router-link to='/students'>students</router-link> | 
+      <router-link to="/login">LOGIN</router-link> |
     </div>
     <router-view />
   </div>
