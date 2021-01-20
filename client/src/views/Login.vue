752f3f97a6c45b5b3e86319293dae8d2ab1bebdd
federicocracco95 <fede.cracco95@gmail.com>
2021-01-20 13:07:33 +0100

    Login completed

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="wrapper" id="login" v-on:submit.prevent>
-    <body class="body-bg pd-12 md:pt-10 pb-6 px-2 md:px-0">
-      <header class="max-b-lg mx-auto text-center">
-        <a href="#">
-          <h1 class="text-4xl font-bold text-black-dark">Login</h1>
-        </a>
-      </header>
+  <div class="body-bg h-screen flex items-center justify-center" id="login" v-on:submit.prevent>
 
       <main
-        class="bg-blue-light max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl"
+        class="bg-blue-light max-w-lg w-full mx-auto p-8 md:p-12 rounded-lg shadow-2xl"
       >
         <section class="text-center">
-          <h3 class="font-bold text-2xl mb-3 text-black-dark">Benvenuto al login!</h3>
-          <p class="text-gray-400 pt-2 font-medium text-black-dark">Effettua l'accesso.</p>
-          <p class="text-black-dark font-medium">
+          <h3 class="text-4xl font-bold text-black-dark mb-8">Login</h3>
+          <p class="text-gray-light pt-2 font-medium">Effettua l'accesso.</p>
+          <p class="text-gray-light font-medium">
             Oppure
             <a
               href="#"
-              class="text-blue-dark font-medium  hover:underline"
+              class="text-blue-dark font-medium hover:underline"
               >Registrati</a
             >
           </p>
@@ -28,44 +22,52 @@
             <div class="flex flex-col mx-auto">
               <div class="mb-6 pt-3 rounded bg-white">
                 <label
-                  class="block text-gray-500 text-sm font-bold mb-2 ml-3"
+                  class="block text-gray-light text-sm font-bold mb-2 ml-3"
                   for="email"
                   >Email</label
                 >
                 <input
                   type="text"
                   id="email"
-                  class="rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3"
+                  class="rounded w-full text-gray-700 focus:outline-none border-b-4 focus:border-blue-dark transition duration-500 px-2 pb-3 pt-2"
+                  :class="{
+                   'border-gray-light': !email,
+                   'border-blue-dark': !!email,
+                  }" 
                   v-model="email"
                   @focus="resetError()"
                 />
               </div>
               <div class="mb-6 pt-3 rounded bg-white">
                 <label
-                  class="block text-gray-500 text-sm font-bold mb-2 ml-3"
+                  class="block text-gray-light text-sm font-bold mb-2 ml-3"
                   for="password"
                   >Password</label
                 >
                 <input
                   type="password"
                   id="password"
-                  class="rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3"
+                  class="rounded w-full text-gray-700 focus:outline-none border-b-4 focus:border-blue-dark transition duration-500 px-2 pb-3 pt-2"
+                  :class="{
+                   'border-gray-light': !password,
+                   'border-blue-dark': !!password,
+                  }" 
                   v-model="password"
                   @focus="resetError()"
                 />
               </div>
             </div>
-            <div class="text-center max-w-lg mx-auto text-red-600 text-sm">
+            <div class="text-center max-w-lg mx-auto text-red-normal text-sm font-bold">
               {{ error }}
             </div>
             <div class="flex justify-center pt-5">
               <button
                 :disabled="btnDisabled"
                 @click="login()"
-                class="focus:outline-none text-white font-bold py-2 px-4 mr-1 mb-1 rounded shadow-lg hover:shadow-xl transition duration-200 flex items-center"
+                class="focus:outline-none text-white-normal font-bold py-2 px-4 mr-1 mb-1 rounded shadow-lg hover:shadow-xl transition duration-200 flex items-center"
                 :class="{
-                   'bg-gray-300 cursor-not-allowed': btnDisabled,
-                   'bg-blue-dark hover:bg-blue-700': !btnDisabled,
+                   'bg-gray-light cursor-not-allowed': btnDisabled,
+                   'bg-blue-dark hover:bg-blue-700 text-white-normal': !btnDisabled,
                 }" 
               >
               <spinner :size="30" class="mr-2" v-if="isLoading"/>
@@ -75,14 +77,13 @@
             <div class="text-center max-w-lg mx-auto pt-10">
               <a
                 href="#"
-                class="text-sm text-blue-600 hover:text-blue-700 hover:underline mb-6"
+                class="text-sm text-blue-dark hover:text-blue-700 hover:underline mb-6"
                 >Hai dimenticato la password?</a
               >
             </div>
           </form>
         </section>
       </main>
-    </body>
   </div>
 </template>
 
