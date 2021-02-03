d12ea5c699afb92f5b736e1f15c1a4c79d045214
Simone <galiottosimone@gmail.com>
2021-02-03 09:25:20 +0100

    update

--- client/src/views/Esame.vue
+++ client/src/views/Esame.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-      <div class="flex py-5">
+      <div class="flex justify-between items-center py-5">
           <h5 class="uppercase pl-5">Esame</h5>
-      </div>
-
-      <div class="flex justify-center mb-5">
-                <div class="border-2 border-blue-500 bg-gray-200 text-blue-500 rounded-full text-center px-4 py-2 m-2" v-for="(q, index) in quizList" :class="{
-                    'active' : index === currentQuizIndex,
-                }" :key="index">
+          <div class="flex justify-center">
+                <div 
+                    class="border-2 border-blue-500 bg-gray-200 text-blue-500 rounded-full text-center px-4 py-2 m-2" 
+                    v-for="(q, index) in quizList"
+                    :class="{
+                        'active' : index === currentQuizIndex,
+                        [checkAnswerByIndex(index)]: true
+                    }" 
+                    :key="index">
                         <h1>{{index + 1}} </h1>
                 </div>
-            </div>
-    <div class="pl-5">
+        </div>
+        <div class="flex items-center justify-center h-24 w-24 border-gray-700 border-4 rounded-full">
+            <h3>{{countdownFormatted}}</h3>
+        </div>
+      </div>
+        
+      
+    <div class="pl-5 mt-2">
 
         <div v-if="currentView === 'quiz'">
 
@@ -67,103 +76,102 @@ export default({
                 {
                     id: 1,
                     quiz: 'Di che colore era il cavallo bianco di napoleone?',
-                    answers: [0],
+                    answers: [],
+                    seen: false,
+                    correct:[1],
                     choices : [
                             {
-                                id : 0, text: 'bianco',
-                                correct: true,
+                                id : 1, text: 'bianco'
                             },
                             {
-                                id : 1, text: 'nero',
-                                correct: false,
+                                id : 2, text: 'nero'
                             },
                             {
-                                id : 2, text: 'grigio',
-                                correct: false,
+                                id : 3, text: 'grigio'
                             },
                             {
-                                id : 3, text: 'bianco',
-                                correct: false,
+                                id : 4, text: 'bianco'
                             },
                         ],
                 },  {
                     id: 2,
                     quiz: 'Quante sono le regioni italiane?',
-                    answers: [0],
+                    answers: [],
+                    seen: false,
+                    correct:[1],
                     choices: [                        
                             {
-                                id : 0, text: '20',
-                                correct: true,
+                                id : 1, text: '20'
                             },
                             {
-                                id : 1, text: '21',
-                                correct: false,
+                                id : 2, text: '21'
                             },
                             {
-                                id : 2, text: '19',
-                                correct: false,
+                                id : 3, text: '19'
                             },
                             {
-                                id : 3, text: '22',
-                                correct: false,
+                                id : 4, text: '22'
                             },
                         ]
                 },  {
                     id: 3,
                     quiz: 'Chi tra questi comanda in ITS Last?',
-                    answers: [2],
+                    answers: [],
+                    seen: false,
+                    correct:[3],
                     choices: [
                         {
-                            id : 0, text: 'Giacomo Alberini',
-                            correct: false,
+                            id : 1, text: 'Giacomo Alberini'
                         },
                         {
-                            id : 1, text: 'Sara Vedovato',
-                            correct: false,
+                            id : 2, text: 'Sara Vedovato'
                         },
                         {
-                            id :2, text: 'Zavaah',
-                            correct: true,
+                            id :3, text: 'Zavaah'
                         },
                         {
-                            id : 3, text: 'Massimo Piazza',
-                            correct: false,
+                            id : 4, text: 'Massimo Piazza'
                         },
                     ],
                 },  {
                     id: 4,
                     quiz: 'Quanto fa: 15 + 18?',
-                    answers: [0],
+                    answers: [],
+                    seen: false,
+                    correct:[4],
                     choices: [
                         
                             {
-                                id : 0, text: '36',
-                                correct: true,
+                                id : 1, text: '36'
                             },
                             {
-                                id : 1, text: '33',
-                                correct: false,
+                                id : 2, text: '33'
                             },
                             {
-                                id : 2, text: 'un pesce',
-                                correct: false,
+                                id : 3, text: 'un pesce'
                             },
+                            {
+                                id : 4, text: 'te pituro sul muro'
+
+                            }
                         ]
                 }
             ],
-            currentQuizIndex: null,
+            currentQuizIndex: 0,
             currentView: 'quiz',
             score: 0,
             risposte: {},
+            countdown: 3600000
         }
     },
     beforeMount() {
-        this.currentQuizIndex = 0;
+        this.setCountdown();
     },
     methods: {
         avanza(){
             console.log(this.currentQuizIndex)
             if(this.currentQuizIndex < this.quizList.length - 1){
+                this.quizList[this.currentQuizIndex].seen = true;
                 this.currentQuizIndex ++;
             }
         },
@@ -171,15 +179,14 @@ export default({
             this.getScore();
         },
         retrocedi() {
+            this.quizList[this.currentQuizIndex].seen = true;
             this.currentQuizIndex = this.currentQuizIndex > 0 ? this.currentQuizIndex - 1 : this.currentQuizIndex;
         },
         getScore(){
-            this.currentView = 'result'; 
-            
+            this.currentView = 'result';
             this.quizList.forEach(quiz => {
-               this.score += quiz.answers.indexOf(this.risposte[quiz.id]) > -1 ? 1 : 0;
-            });
-            
+               this.score += quiz.correct.indexOf(this.risposte[quiz.id]) > -1 ? 1 : 0;
+            });            
             
         },
         newGame(){
@@ -188,7 +195,25 @@ export default({
             this.currentView = 'quiz';
             
         },
-        
+        checkAnswerByIndex(index) {
+            let quizElement = this.quizList[index]
+            if (!this.risposte[quizElement.id] && quizElement.seen) {
+                return 'bg-red-200';
+            } else if (!this.risposte[quizElement.id]) {
+                return 'bg-gray-200';
+            } else {
+                return 'bg-green-200';
+            }
+        },
+        setCountdown() {
+            let data = this;
+            var x = setInterval(function() {
+                if (data.countdown == 0) {
+                    clearInterval(x);
+                }   
+                data.countdown -= 1000
+            }, 1000);
+        }
     },
     computed: {
         currentQuiz(){
@@ -199,11 +224,11 @@ export default({
         },
         endButtonLabel(){
             return this.currentQuizIndex != this.quizList.length -1;
+        },
+        countdownFormatted() {
+            return Math.floor(this.countdown / 60000) + ':' + ((this.countdown % 60000) / 1000).toFixed(0);
         }
 
     }
 })
 </script>
-<style>
-    
-</style>
